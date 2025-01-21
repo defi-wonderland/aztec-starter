07631efaf888339015c9ab3d4433a966ed613b3c
Weißer Hase <wei3erHase@protonmail.com>
2025-01-21 17:02:07 +0100

    chore: adding different uint notes for benchmark

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,89 +1,89 @@
-import { EasyPrivateVotingContractArtifact, EasyPrivateVotingContract } from "../artifacts/EasyPrivateVoting.js"
-import { AccountWallet, CompleteAddress, ContractDeployer, createLogger, Fr, PXE, waitForPXE, TxStatus, createPXEClient, getContractInstanceFromDeployParams, Logger } from "@aztec/aztec.js";
-import { getInitialTestAccountsWallets } from "@aztec/accounts/testing"
+// import { EasyPrivateVotingContractArtifact, EasyPrivateVotingContract } from "../artifacts/EasyPrivateVoting.js"
+// import { AccountWallet, CompleteAddress, ContractDeployer, createLogger, Fr, PXE, waitForPXE, TxStatus, createPXEClient, getContractInstanceFromDeployParams, Logger } from "@aztec/aztec.js";
+// import { getInitialTestAccountsWallets } from "@aztec/accounts/testing"
 
-const setupSandbox = async () => {
-    const { PXE_URL = 'http://localhost:8080' } = process.env;
-    const pxe = createPXEClient(PXE_URL);
-    await waitForPXE(pxe);
-    return pxe;
-};
+// const setupSandbox = async () => {
+//     const { PXE_URL = 'http://localhost:8080' } = process.env;
+//     const pxe = createPXEClient(PXE_URL);
+//     await waitForPXE(pxe);
+//     return pxe;
+// };
 
-describe("Voting", () => {
-    let pxe: PXE;
-    let wallets: AccountWallet[] = [];
-    let accounts: CompleteAddress[] = [];
-    let logger: Logger;
+// describe("Voting", () => {
+//     let pxe: PXE;
+//     let wallets: AccountWallet[] = [];
+//     let accounts: CompleteAddress[] = [];
+//     let logger: Logger;
 
-    beforeAll(async () => {
-        logger = createLogger('aztec:aztec-starter');
-        logger.info("Aztec-Starter tests running.")
+//     beforeAll(async () => {
+//         logger = createLogger('aztec:aztec-starter');
+//         logger.info("Aztec-Starter tests running.")
 
-        pxe = await setupSandbox();
+//         pxe = await setupSandbox();
 
-        wallets = await getInitialTestAccountsWallets(pxe);
-        accounts = wallets.map(w => w.getCompleteAddress())
-    })
+//         wallets = await getInitialTestAccountsWallets(pxe);
+//         accounts = wallets.map(w => w.getCompleteAddress())
+//     })
 
-    it("Deploys the contract", async () => {
-        const salt = Fr.random();
-        const VotingContractArtifact = EasyPrivateVotingContractArtifact
-        const [deployerWallet, adminWallet] = wallets; // using first account as deployer and second as contract admin
-        const adminAddress = adminWallet.getCompleteAddress().address;
+//     it("Deploys the contract", async () => {
+//         const salt = Fr.random();
+//         const VotingContractArtifact = EasyPrivateVotingContractArtifact
+//         const [deployerWallet, adminWallet] = wallets; // using first account as deployer and second as contract admin
+//         const adminAddress = adminWallet.getCompleteAddress().address;
 
-        const deploymentData = getContractInstanceFromDeployParams(VotingContractArtifact,
-            {
-                constructorArgs: [adminAddress],
-                salt,
-                deployer: deployerWallet.getAddress()
-            });
-        const deployer = new ContractDeployer(VotingContractArtifact, deployerWallet);
-        const tx = deployer.deploy(adminAddress).send({ contractAddressSalt: salt })
-        const receipt = await tx.getReceipt();
+//         const deploymentData = getContractInstanceFromDeployParams(VotingContractArtifact,
+//             {
+//                 constructorArgs: [adminAddress],
+//                 salt,
+//                 deployer: deployerWallet.getAddress()
+//             });
+//         const deployer = new ContractDeployer(VotingContractArtifact, deployerWallet);
+//         const tx = deployer.deploy(adminAddress).send({ contractAddressSalt: salt })
+//         const receipt = await tx.getReceipt();
 
-        expect(receipt).toEqual(
-            expect.objectContaining({
-                status: TxStatus.PENDING,
-                error: ''
-            }),
-        );
+//         expect(receipt).toEqual(
+//             expect.objectContaining({
+//                 status: TxStatus.PENDING,
+//                 error: ''
+//             }),
+//         );
 
-        const receiptAfterMined = await tx.wait({ wallet: deployerWallet });
+//         const receiptAfterMined = await tx.wait({ wallet: deployerWallet });
 
-        expect(await pxe.getContractInstance(deploymentData.address)).toBeDefined();
-        expect(await pxe.isContractPubliclyDeployed(deploymentData.address)).toBeTruthy();
-        expect(receiptAfterMined).toEqual(
-            expect.objectContaining({
-                status: TxStatus.SUCCESS,
-            }),
-        );
+//         expect(await pxe.getContractInstance(deploymentData.address)).toBeDefined();
+//         expect(await pxe.isContractPubliclyDeployed(deploymentData.address)).toBeTruthy();
+//         expect(receiptAfterMined).toEqual(
+//             expect.objectContaining({
+//                 status: TxStatus.SUCCESS,
+//             }),
+//         );
 
-        expect(receiptAfterMined.contract.instance.address).toEqual(deploymentData.address)
-    }, 300_000)
+//         expect(receiptAfterMined.contract.instance.address).toEqual(deploymentData.address)
+//     }, 300_000)
 
-    it("It casts a vote", async () => {
-        const candidate = new Fr(1)
+//     it("It casts a vote", async () => {
+//         const candidate = new Fr(1)
 
-        const contract = await EasyPrivateVotingContract.deploy(wallets[0], accounts[0].address).send().deployed();
-        const tx = await contract.methods.cast_vote(candidate).send().wait();
-        let count = await contract.methods.get_vote(candidate).simulate();
-        expect(count).toBe(1n);
-    }, 300_000)
+//         const contract = await EasyPrivateVotingContract.deploy(wallets[0], accounts[0].address).send().deployed();
+//         const tx = await contract.methods.cast_vote(candidate).send().wait();
+//         let count = await contract.methods.get_vote(candidate).simulate();
+//         expect(count).toBe(1n);
+//     }, 300_000)
 
-    it("It should fail when trying to vote twice", async () => {
-        const candidate = new Fr(1)
+//     it("It should fail when trying to vote twice", async () => {
+//         const candidate = new Fr(1)
 
-        const contract = await EasyPrivateVotingContract.deploy(wallets[0], accounts[0].address).send().deployed();
-        await contract.methods.cast_vote(candidate).send().wait();
+//         const contract = await EasyPrivateVotingContract.deploy(wallets[0], accounts[0].address).send().deployed();
+//         await contract.methods.cast_vote(candidate).send().wait();
 
-        // We try voting again, but our TX is dropped due to trying to emit duplicate nullifiers
-        // first confirm that it fails simulation
-        await expect(contract.methods.cast_vote(candidate).send().wait()).rejects.toThrow(/Nullifier collision/);
-        // if we skip simulation, tx is dropped
-        await expect(
-            contract.methods.cast_vote(candidate).send({ skipPublicSimulation: true }).wait(),
-        ).rejects.toThrow('Reason: Tx dropped by P2P node.');
+//         // We try voting again, but our TX is dropped due to trying to emit duplicate nullifiers
+//         // first confirm that it fails simulation
+//         await expect(contract.methods.cast_vote(candidate).send().wait()).rejects.toThrow(/Nullifier collision/);
+//         // if we skip simulation, tx is dropped
+//         await expect(
+//             contract.methods.cast_vote(candidate).send({ skipPublicSimulation: true }).wait(),
+//         ).rejects.toThrow('Reason: Tx dropped by P2P node.');
 
-    }, 300_000)
+//     }, 300_000)
 
-});
+// });
